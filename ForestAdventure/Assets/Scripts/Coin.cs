ea2c39c64707e717d270ad63eea2a6b064f8f2f0
gvidalquadras <202203923@alu.comillas.edu>
2025-01-11 23:17:56 +0100

    Update Coin.cs

--- ForestAdventure/Assets/Scripts/Coin.cs
+++ ForestAdventure/Assets/Scripts/Coin.cs
@@ -2,25 +2,25 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    public int scoreValue = 1; // Valor de la moneda
-    private ScoreManager scoreManager; // Referencia al gestor de puntuación
+    public int scoreValue = 1; 
+    private ScoreManager scoreManager; 
 
     void Start()
     {
-        // Encuentra el ScoreManager en la escena
+        
         scoreManager = FindObjectOfType<ScoreManager>();
         if (scoreManager == null)
         {
-            Debug.LogError("No se encontró un ScoreManager en la escena.");
+            Debug.LogError("No se encontrÃ³ un ScoreManager en la escena.");
         }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        // Comprueba si el objeto que entra pertenece a la capa del jugador
+        // Chechs if object is the player
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            // Agrega puntuación y destruye la moneda
+            
             scoreManager.AddScore(scoreValue);
             Destroy(gameObject);
         }

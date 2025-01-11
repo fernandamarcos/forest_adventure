b5418802d9cd733ea78c10016d7c63e2d60fea5e
gvidalquadras <202203923@alu.comillas.edu>
2025-01-11 23:35:35 +0100

    Update WarriorMovement.cs

--- ForestAdventure/Assets/Scripts/Player/WarriorMovement.cs
+++ ForestAdventure/Assets/Scripts/Player/WarriorMovement.cs
@@ -6,7 +6,7 @@ public class WarriorMovement : MonoBehaviour
 {
     public float JumpForce;
     public float Speed;
-    public float FallMultiplier = 2.5f; // Incrementa la gravedad al caer
+    public float FallMultiplier = 2.5f; 
     private Rigidbody2D Rigidbody2D;
     private float Horizontal;
     private bool grounded;
@@ -26,10 +26,10 @@ public class WarriorMovement : MonoBehaviour
     {
         Horizontal = Input.GetAxisRaw("Horizontal");
 
-        // Dibujar el rayo para depuración
+        
         Debug.DrawRay(transform.position, Vector3.down * 0.8f, Color.red);
 
-        // Voltear el personaje según la dirección
+        
         if (Horizontal > 0)
         {
             transform.localScale = new Vector3(4, 4, 4); // Escala normal
@@ -39,16 +39,16 @@ public class WarriorMovement : MonoBehaviour
             transform.localScale = new Vector3(-4, 4, 4); // Invierte el eje X
         }
 
-        // Activar animación de correr
+        
         animator.SetBool("running", Horizontal != 0.0f);
 
-        // Verificar si está en el suelo
+        
         grounded = Physics2D.Raycast(transform.position, Vector3.down, 0.8f);
 
-        // Activar/desactivar animación de salto
+        
         animator.SetBool("jumping", !grounded);
 
-        // Si está en el suelo y se presiona W, salta
+        
         if (Input.GetKeyDown(KeyCode.W) && grounded)
         {
             Jump();
@@ -57,16 +57,16 @@ public class WarriorMovement : MonoBehaviour
 
     private void Jump()
     {
-        Rigidbody2D.velocity = new Vector2(Rigidbody2D.velocity.x, 0); // Reinicia la velocidad vertical
+        Rigidbody2D.velocity = new Vector2(Rigidbody2D.velocity.x, 0); 
         Rigidbody2D.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
     }
 
     private void FixedUpdate()
     {
-        // Movimiento horizontal
+        
         Rigidbody2D.velocity = new Vector2(Horizontal * Speed, Rigidbody2D.velocity.y);
 
-        // Incrementa la gravedad si el personaje está cayendo
+        // Increase gravity if player is falling
         if (Rigidbody2D.velocity.y < 0)
         {
             Rigidbody2D.velocity += Vector2.up * Physics2D.gravity.y * (FallMultiplier - 1) * Time.fixedDeltaTime;

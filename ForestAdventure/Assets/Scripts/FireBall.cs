cffbcec8cfe9a8c83e548e2a38c4dab6ec2db3b2
gvidalquadras <202203923@alu.comillas.edu>
2025-01-11 23:21:24 +0100

    Update FireBall.cs

--- ForestAdventure/Assets/Scripts/FireBall.cs
+++ ForestAdventure/Assets/Scripts/FireBall.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class Fireball : Enemy
 {
-    [SerializeField] private float fireballSpeed = 10f;  // Velocidad de disparo
-    [SerializeField] private float lifetime = 5f;  // Tiempo de vida de la bola de fuego antes de ser destruida
-    [SerializeField] private Rigidbody2D rb;  // Rigidbody2D de la bola de fuego
-    [SerializeField] private Collider2D col;  // Collider2D de la bola de fuego
-    [SerializeField] private PhysicsMaterial2D bounceMaterial; // Material de física para el rebote
+    [SerializeField] private float fireballSpeed = 10f;  
+    [SerializeField] private float lifetime = 5f;  
+    [SerializeField] private Rigidbody2D rb;  
+    [SerializeField] private Collider2D col; 
+    [SerializeField] private PhysicsMaterial2D bounceMaterial; 
 
     protected override void Start()
     {
@@ -14,9 +14,9 @@ public class Fireball : Enemy
         rb = GetComponent<Rigidbody2D>();
         col = GetComponent<Collider2D>();
 
-        // Asignamos el material de rebote
+        // Assign bounce material
         col.sharedMaterial = bounceMaterial;
-        // Destruye la bola de fuego después de un tiempo
+        //Destroy ball after some time
         Destroy(gameObject, lifetime);
     }
 
@@ -28,7 +28,7 @@ public class Fireball : Enemy
     protected override void OnCollisionEnter2D(Collision2D collision)
     {
         base.OnCollisionEnter2D(collision);
-        if (collision.gameObject.CompareTag("Player")) // Verificar si el objeto es el jugador
+        if (collision.gameObject.CompareTag("Player")) // Check if object is player
         {
             Die();
         }
